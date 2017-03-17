5baafd72275221efad86276535b6eed223b2cc45
Dave Rich <dave@daverich.net>
2017-03-17 09:47:28 -0400

    Added check to see if Lync is already running

--- open-lync.sh
+++ open-lync.sh
@@ -5,7 +5,12 @@ SSID=$(networksetup -getairportnetwork en0 | awk -F ': ' '{print $2}')
 # Check if the current SSID to determine if I'm connected at network
 if [ "$SSID" == "wireless.expedient.com" ]; then
 
-  # Run lync
-  "/Applications/Microsoft Lync.app/Contents/MacOS/Microsoft Lync"
+  # Make sure lync isn't already running
+  LYNC_PROCESSES=$(ps -ef | grep "/Applications/Microsoft Lync.app/Contents/MacOS/Microsoft Lync" | grep -v grep | wc -l)
+
+  if [ $LYNC_PROCESSES -eq 0 ]; then
+    # Run lync
+    "/Applications/Microsoft Lync.app/Contents/MacOS/Microsoft Lync"
+  fi
 
 fi
